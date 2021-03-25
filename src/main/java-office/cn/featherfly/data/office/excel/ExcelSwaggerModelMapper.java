3c0405bc68f8e1dee7351ad158173eaaa4293f93
zhongji <featherfly@foxmail.com>
2021-03-25 12:50:08 +0800

    让ExcelSwaggerModelMapper自动加入的标题行的标题居中

--- src/main/java-office/cn/featherfly/data/office/excel/ExcelSwaggerModelMapper.java
+++ src/main/java-office/cn/featherfly/data/office/excel/ExcelSwaggerModelMapper.java
@@ -4,6 +4,8 @@ package cn.featherfly.data.office.excel;
 import java.util.Collection;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.Row;
 
 import cn.featherfly.common.bean.BeanDescriptor;
@@ -15,16 +17,14 @@ import io.swagger.annotations.ApiModelProperty;
  * <p>
  * ExcelSwaggerModelMapper
  * </p>
- * 
- * @param <R>
- *            要映射的具体类
+ *
+ * @param <R> 要映射的具体类
  * @author 钟冀
  */
 public class ExcelSwaggerModelMapper<R> extends ExcelDataMapper<R> {
 
     private boolean insertTitleRow = true;
-    
-    
+
     /**
      */
     public ExcelSwaggerModelMapper() {
@@ -32,8 +32,7 @@ public class ExcelSwaggerModelMapper<R> extends ExcelDataMapper<R> {
     }
 
     /**
-     * @param insertTitleRow
-     *            insertTitleRow
+     * @param insertTitleRow insertTitleRow
      */
     public ExcelSwaggerModelMapper(boolean insertTitleRow) {
         this.insertTitleRow = insertTitleRow;
@@ -54,23 +53,25 @@ public class ExcelSwaggerModelMapper<R> extends ExcelDataMapper<R> {
     public void fillData(Row row, R record, int rowNum) {
         if (record != null) {
             BeanDescriptor<?> beanDescriptor = BeanDescriptor.getBeanDescriptor(record.getClass());
-            
+
             Collection<BeanProperty<?>> beanProperties = getApiModelBeanProperties(beanDescriptor);
             int index = 0;
             if (insertTitleRow && rowNum == 0) {
+                CellStyle cellStyle = row.getSheet().getWorkbook().createCellStyle();
+                cellStyle.setAlignment(HorizontalAlignment.CENTER);
                 // 第一行的时候再把标题添加进去
                 for (BeanProperty<?> beanProperty : beanProperties) {
-                    ApiModelProperty apiModelProperty = (ApiModelProperty) beanProperty
-                            .getAnnotation(ApiModelProperty.class);                
+                    ApiModelProperty apiModelProperty = beanProperty.getAnnotation(ApiModelProperty.class);
                     Cell cell = row.createCell(index);
+                    cell.setCellStyle(cellStyle);
                     setCellValue(apiModelProperty.value(), cell);
                     index++;
                 }
                 row = row.getSheet().createRow(rowNum + 1);
                 index = 0;
             }
-            
-            for (BeanProperty<?> beanProperty : beanProperties) {                
+
+            for (BeanProperty<?> beanProperty : beanProperties) {
                 Cell cell = row.createCell(index);
                 Object object = beanProperty.getValue(record);
                 setCellValue(object, cell);
@@ -81,7 +82,7 @@ public class ExcelSwaggerModelMapper<R> extends ExcelDataMapper<R> {
 
     /**
      * 返回insertTitleRow
-     * 
+     *
      * @return insertTitleRow
      */
     public boolean isInsertTitleRow() {
@@ -90,14 +91,13 @@ public class ExcelSwaggerModelMapper<R> extends ExcelDataMapper<R> {
 
     /**
      * 设置insertTitleRow
-     * 
-     * @param insertTitleRow
-     *            insertTitleRow
+     *
+     * @param insertTitleRow insertTitleRow
      */
     public void setInsertTitleRow(boolean insertTitleRow) {
         this.insertTitleRow = insertTitleRow;
     }
-    
+
     private Collection<BeanProperty<?>> getApiModelBeanProperties(BeanDescriptor<?> beanDescriptor) {
         return beanDescriptor.findBeanPropertys(new BeanPropertyAnnotationMatcher(ApiModelProperty.class));
     }
